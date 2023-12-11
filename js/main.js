74fad9d74691961d3a9e5fd57d1b26962e35ef46
SANTIAGOROCHA777 <127887218+SANTIAGOROCHA777@users.noreply.github.com>
2023-12-11 15:51:09 -0500

    Tercer Commit
    3

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const btn2_ctn = document.getElementsByClassName("btn2_container")[0];
+const two = document.getElementsByClassName("two")[0];
+btn2_ctn.addEventListener("click", () => {
+  two.classList.toggle("inactive2");
+});
+
+const btn3_ctn = document.getElementsByClassName("btn3_container")[0];
+const tres = document.getElementsByClassName("tres")[0];
+btn3_ctn.addEventListener("click", () => {
+  tres.classList.toggle("inactive3");
+});
+
+const btn4_ctn = document.getElementsByClassName("btn4_container")[0];
+const four = document.getElementsByClassName("four")[0];
+btn4_ctn.addEventListener("click", () => {
+  four.classList.toggle("inactive4");
+});
+
+const btn5_ctn = document.getElementsByClassName("btn5_container")[0];
+const five = document.getElementsByClassName("five")[0];
+btn5_ctn.addEventListener("click", () => {
+  five.classList.toggle("inactive5");
+});
+
+const btn6_ctn = document.getElementsByClassName("btn6_container")[0];
+const six = document.getElementsByClassName("six")[0];
+btn6_ctn.addEventListener("click", () => {
+  six.classList.toggle("inactive6");
+});
+
+const btn7_ctn = document.getElementsByClassName("btn7_container")[0];
+const seven = document.getElementsByClassName("seven")[0];
+btn7_ctn.addEventListener("click", () => {
+  seven.classList.toggle("inactive7");
+});
